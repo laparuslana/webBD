bbe08f8913cab81d741497d13df10b0ec80ffcc5
laparuslana <90132117+laparuslana@users.noreply.github.com>
2022-12-23 22:52:10 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Header />
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Header from "./components/layouts/Header.vue";
+import Footer from "./components/layouts/Footer.vue";
+
+export default defineComponent({
+  components: {
+    Header,
+    Footer,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
